850412589a74e4183398d1627178d47b0fd2d997
Guy Boertje <guy@elastic.co>
2018-07-01 05:14:27 +0100

    rename rspec run tag in ci/build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -17,5 +17,5 @@ else
     echo "Running default build scripts in: `pwd`/ci/build.sh"
     bundle install
     bundle exec rake vendor
-    bundle exec rspec --tag ~lsof spec
+    bundle exec rspec --tag ~unix spec
 fi
