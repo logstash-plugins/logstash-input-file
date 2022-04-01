4923614f084ad0bb55c20125ff877bd11c79548e
andsel <selva.andre@gmail.com>
2022-04-01 15:20:06 +0200

    Added test to verify that ReadFile handler doesn't consider the value stored in sincedb

--- spec/filewatch/read_mode_handlers_read_file_spec.rb
+++ spec/filewatch/read_mode_handlers_read_file_spec.rb
@@ -36,5 +36,45 @@ module FileWatch
         processor.read_file(watched_file)
       end
     end
+
+    context "when restart from existing sincedb" do
+      let(:settings) do
+        Settings.from_options(
+          :sincedb_write_interval => 0,
+          :sincedb_path => File::NULL,
+          :file_chunk_size => 10
+        )
+      end
+
+      let(:processor) { double("fake processor") }
+      let(:observer) { TestObserver.new }
+      let(:watch) { double("watch") }
+
+      before(:each) {
+        allow(watch).to receive(:quit?).and_return(false)#.and_return(false).and_return(true)
+        allow(processor).to receive(:watch).and_return(watch)
+      }
+
+      it "read from where it left" do
+        listener = observer.listener_for(Pathname.new(pathname).to_path)
+        sut = ReadMode::Handlers::ReadFile.new(processor, sdb_collection, observer, settings)
+
+        # simulate a previous partial read of the file
+        sincedb_value = SincedbValue.new(0)
+        sincedb_value.set_watched_file(watched_file)
+        sdb_collection.set(watched_file.sincedb_key, sincedb_value)
+
+
+        # simulate a consumption of first line, (size + newline) bytes
+        sdb_collection.increment(watched_file.sincedb_key, File.readlines(pathname)[0].size + 2)
+
+        # exercise
+        sut.handle(watched_file)
+
+        # verify
+        expect(listener.lines.size).to eq(1)
+        expect(listener.lines[0]).to start_with("2010-03-12   23:51:21   SEA4   192.0.2.222   play   3914   OK")
+      end
+    end
   end
 end
